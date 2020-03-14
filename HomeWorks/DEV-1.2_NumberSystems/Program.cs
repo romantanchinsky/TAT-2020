ab86772e604aa7573b50f3e212bd8ddf297d1e27
Roman Tanchinsky <ser.tanchens@gmail.com>
2020-03-14 23:31:35 +0300

    task complited (without tests)

---
+++ HomeWorks/DEV-1.2_NumberSystems/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1._2_NumberSystems
+{
+    class Program
+    {
+        static void Main ( string [] args )
+        {
+            Caller();
+        }
+
+        static void Caller ()
+        {
+            string exetString = "y";
+            int number;
+            int numberSystem;
+            while ( exetString == "y")
+            {
+                while ( true )
+                {
+                    Console.WriteLine("Enter decimal number");
+                    if ( int.TryParse(Console.ReadLine(), out number) ) { break; }
+                }
+                while ( true )
+                {
+                    Console.WriteLine("Enter number system");
+                    if ( int.TryParse(Console.ReadLine(), out numberSystem) && numberSystem >= 2 && numberSystem <= 20 ) { break; }
+                }
+                SystemConverter systemConverter = new SystemConverter();
+                Console.WriteLine(systemConverter.Convert(number, numberSystem));
+                Console.WriteLine("Do you what to contuine (y/n): ");
+                exetString = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ HomeWorks/DEV-1.2_NumberSystems/SystemConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DEV_1._2_NumberSystems
+{
+    public class SystemConverter
+    {
+        public const string EXCEPTION_CONVERT_MESSAGE = "Cant convert elements > 19";
+
+        protected const char ZERO = '0';
+
+        internal string Convert ( int number, int baseSystem )
+        {
+            List<int> result = new List<int>();
+            bool isNegative = false;
+            if ( number < 0 )
+            {
+                number *= -1;
+                isNegative = true;
+
+            }
+            for ( int i = 0; number != 0 ; i++)
+            {
+                result.Add(number % baseSystem);
+                number /= baseSystem;
+            }
+            return ConvertArray(result.ToArray(), isNegative);
+        }
+
+        protected string ConvertArray ( int [] number, bool isNegative )
+        {
+            StringBuilder outString = new StringBuilder();
+            if ( isNegative ) { outString.Append('-'); }
+            for ( int i = number.Length - 1; i >= 0; i-- )
+            {
+                outString.Append(ConvertElement(number [ i ]));
+            }
+            return outString.ToString();
+        }
+
+        protected char ConvertElement ( int element )     // converts number from 0 to 19 to correct char symbol
+        {
+            if ( element < 10 ) { return (char)(element + ZERO); }
+            switch ( element )
+            {
+                case 10:
+                    return 'A';
+                case 11:
+                    return 'B';
+                case 12:
+                    return 'C';
+                case 13:
+                    return 'D';
+                case 14:
+                    return 'E';
+                case 15:
+                    return 'F';
+                case 16:
+                    return 'G';
+                case 17:
+                    return 'H';
+                case 18:
+                    return 'I';
+                case 19:
+                    return 'J';
+            }
+            throw new ArgumentOutOfRangeException(EXCEPTION_CONVERT_MESSAGE);
+        }
+    }
+}
