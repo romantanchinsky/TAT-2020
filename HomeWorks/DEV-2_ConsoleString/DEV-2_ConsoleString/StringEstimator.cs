a0f6536eba2f711c15daaa5c8b489a0ec318ff07
Roman Tanchinsky <ser.tanchens@gmail.com>
2020-04-08 15:08:04 +0300

    DEV-1 code refactoring

--- HomeWorks/DEV-2_ConsoleString/DEV-2_ConsoleString/StringEstimator.cs
+++ HomeWorks/DEV-2_ConsoleString/DEV-2_ConsoleString/StringEstimator.cs
@@ -4,25 +4,35 @@ namespace DEV_2_ConsoleString
 {
     public class StringEstimator
     {
-        public const string ARGUMENT_NULL_EXCEPTINO_MESSSAGE = "Argument == null";
-        public int GetMaxSequence ( string theString )
+        public const string ARGUMENT_NULL_EXCEPTION_MESSAGE = "Argument == null";
+        public int GetMaxSequence ( string checkedString )
         {
-            if ( theString == null ) { throw new ArgumentNullException(ARGUMENT_NULL_EXCEPTINO_MESSSAGE); }
-            if ( theString.Length > 0  )
+            if ( checkedString == null )
             {
-                int answer = 1, temp = 1;
-                for ( int i = 1; i < theString.Length; i++ )
-                {
-                    if ( theString [ i ] != theString [ i - 1 ] )
-                    {
-                        if ( answer < temp ) { answer = temp; }
-                        temp = 1;
-                    }
-                    else { temp++; }
-                }
-                return answer > temp ? answer : temp;
+                throw new ArgumentNullException(ARGUMENT_NULL_EXCEPTION_MESSAGE);
             }
-            return 0;
+            if ( checkedString.Length > 1  )
+            {
+                int maxLength = 1, 
+                    currentLength = 1;
+                for ( int i = 1; i < checkedString.Length; i++ )
+                {
+                    if ( checkedString [ i ] != checkedString [ i - 1 ] )
+                    {
+                        if ( maxLength < currentLength )
+                        { 
+                            maxLength = currentLength;
+                        }
+                        currentLength = 1;
+                    }
+                    else 
+                    { 
+                        currentLength++; 
+                    }
+                }
+                return maxLength > currentLength ? maxLength : currentLength;
+            }
+            return checkedString == String.Empty ?  0 : 1;
         }
     }
 }
